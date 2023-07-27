69dd55589a596a07b5f49bdc7adbbf38a6cce7c1
박주헌 <741u741@naver.com>
2023-07-27 23:18:44 +0900

    20230727_2318

--- self/20230727/problem_10989.py
+++ self/20230727/problem_10989.py
@@ -1,10 +1,12 @@
-N = int(input())
-problem_list = []
+import sys
+N = int(sys.stdin.readline())
+ans_list = [0] * 10001
 
 for _ in range(N):
-    problem_list.append(int(input()))
-
-problem_list.sort()
-
-for i in problem_list:
-    print(i)
+    where = int(sys.stdin.readline())
+    ans_list[where] += 1
+    
+for i in range(len(ans_list)):
+    
+    for j in range(ans_list[i]):
+        print(i)
