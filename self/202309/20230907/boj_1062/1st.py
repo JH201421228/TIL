472823324222c8095d53e4e81fabeea324fb0823
박주헌 <741u741@naver.com>
2023-09-07 22:44:52 +0900

    20230907 #1

---
+++ self/202309/20230907/boj_1062/1st.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+
+
+
+N, K = map(int, input().split())
+check_list = ['a', 'n', 't', 'i', 'c']
+str_list = []
+for _ in range(N):
+    input_str = input()
+    input_str = input_str[4:]
+    input_str = input_str[:-4]
+    str_list.append(input_str)
+print(str_list)
+censored_str_list = []
+for
+if N < 5:
+    print(0)
+else:
