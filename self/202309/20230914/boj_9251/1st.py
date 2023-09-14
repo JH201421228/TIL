53837c8ac5a0bbd75cd595abd66f2fda8a775fab
박주헌 <741u741@naver.com>
2023-09-14 21:57:30 +0900

    20230914 #2

--- self/202309/20230914/boj_9251/1st.py
+++ self/202309/20230914/boj_9251/1st.py
@@ -3,5 +3,14 @@ sys.stdin = open('input.txt')
 
 first_list = list(input())
 second_list = list(input())
-print(first_list, second_list)
-check_list = [1] * len(first_list)
+# print(first_list, second_list)
+length1 = len(first_list)+1
+length2 = len(second_list)+1
+check_list = [[0] * length2 for _ in range(length1)]
+for i in range(1, length1):
+    for j in range(1, length2):
+        if first_list[i-1] == second_list[j-1]:
+            check_list[i][j] = check_list[i-1][j-1] + 1
+        else:
+            check_list[i][j] = max(check_list[i][j-1], check_list[i-1][j])
+print(check_list[-1][-1])
