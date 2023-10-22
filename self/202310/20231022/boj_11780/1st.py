0ed7e6880f499443ffd154e1669aae329baffbdd
박주헌 <741u741@naver.com>
2023-10-22 22:25:34 +0900

    20231022 #3

--- self/202310/20231022/boj_11780/1st.py
+++ self/202310/20231022/boj_11780/1st.py
@@ -3,9 +3,17 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 
+def path_finder(i, j):
+    if not visited[i][j]:
+        return []
+
+    n = visited[i][j]
+    return path_finder(i, n) + [n] + path_finder(n, j)
+
+
 N = int(input())
 graph = [[float('inf')] * (N+1) for _ in range(N+1)]
-visited = [[[] for _ in range(N+1)] for _ in range(N+1)]
+visited = [[0] * (N+1) for _ in range(N+1)]
 for _ in range(int(input())):
     start, end, cost = map(int, input().split())
     graph[start][end] = min(graph[start][end], cost)
@@ -15,9 +23,23 @@ for k in range(1, N+1):
         for j in range(1, N+1):
             if i == j:
                 graph[i][i] = 0
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+            elif graph[i][j] > graph[i][k] + graph[k][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+                visited[i][j] = k
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if graph[i][j] == float('inf'):
+            graph[i][j] = 0
+
 for inner in graph[1:]:
     print(*inner[1:])
-# for inner in visited:
-#     print(inner)
-print(visited[1][1])
+# for inner in visited[1:]:
+#     print(inner[1:])
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if not graph[i][j]:
+            print(0)
+        else:
+            ans = [i] + path_finder(i, j) + [j]
+            print(len(ans), *ans)
