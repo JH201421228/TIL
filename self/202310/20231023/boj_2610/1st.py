bd4c919561dafcecf8bfc87501399d1609ceecd0
박주헌 <741u741@naver.com>
2023-10-23 18:01:40 +0900

    20231023 #2

--- self/202310/20231023/boj_2610/1st.py
+++ self/202310/20231023/boj_2610/1st.py
@@ -2,8 +2,91 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
+# 가운데 값을 조회하는 재귀 함수 만들기
+# 조회하면서 visited함수 만들고
+# 몇개의 파티가 있는지 세면
+
+
+def party_maker(n):
+    ans = [n]
+    visit[n] = 1
+    stack = [n]
+    while stack:
+        now = stack.pop()
+        for next in range(1, N+1):
+            if graph[now][next] and not visit[next]:
+                visit[next] = 1
+                stack.append(next)
+                ans.append(next)
+    return ans
+
 
 N = int(input())
 M = int(input())
 
-graph = [[0]]
+graph = [[float('inf')] * (N+1) for _ in range(N+1)]
+visited = [[0] * (N+1) for _ in range(N+1)]
+for _ in range(M):
+    start, end = map(int, input().split())
+    graph[start][end] = graph[end][start] = 1
+
+# for inner in graph[1:]:
+#     print(inner[1:])
+# print('-------------------------')
+
+for k in range(1, N+1):
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            if i == j:
+                graph[i][i] = 0
+            elif graph[i][j] > graph[i][k] + graph[k][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+                # visited[i][j] = k
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if graph[i][j] == float('inf'):
+            graph[i][j] = 0
+
+for inner in graph[1:]:
+    print(inner[1:])
+print('-------------------------')
+# for inner in visited[1:]:
+#     print(inner[1:])
+# # visited = [0] * (N+1)
+# for i in range(1, N+1):
+#     for j in range(1, N+1):
+#         if not visited[i][j]:
+#             continue
+#         else:
+#             print([i] + path_finder(i, j) + [j])
+# print('------------------')
+visit = [0] * (N+1)
+ans = []
+for i in range(1, N+1):
+    if not visit[i]:
+        ans.append(party_maker(i))
+print(ans)
+# for inner in ans:
+#     max_val = 0
+#     for i in inner:
+#         for j in inner:
+#             max_val = max(max_val, graph[i][j], graph[j][i])
+check = []
+for i in range(1, N+1):
+    temp = 0
+    for j in range(1, N+1):
+        temp += (graph[i][j] + graph[j][i])
+    check.append(temp)
+# print(check)
+print(len(ans))
+for inner in ans:
+    temp = []
+    for num in inner:
+        temp.append(sum(graph[num][1:]))
+    if len(temp) > 1:
+        print(temp)
+        min = min(temp)
+        print(temp.index(min))
+    else:
+        print(temp)
