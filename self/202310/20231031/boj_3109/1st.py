9cccd36c8cde04ffde407f4bb92b13176a17aa1e
박주헌 <741u741@naver.com>
2023-10-31 23:52:53 +0900

    20231031 #1

--- self/202310/20231031/boj_3109/1st.py
+++ self/202310/20231031/boj_3109/1st.py
@@ -1,5 +1,4 @@
 import sys
-
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
@@ -7,20 +6,26 @@ input = sys.stdin.readline
 delta = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
 
 
-def dfs(x, y):
+def dfs(x, y, status):
     global flag
     if y == C-1:
         flag = 1
         return
 
     for dx, dy in delta:
-        if 0 <= x+dx < R and 0 <= y+dy < C and not visited[x+dx][y+dy] and MAP[x+dx][y+dy] == '.':
-            visited[x+dx][y+dy] = 1
-            dfs(x+dx, y+dy)
+        if status and dx == -1:
+            continue
+
+        if 0 <= x+dx < R and 0 <= y+dy < C and MAP[x+dx][y+dy] == '.':
+            MAP[x+dx][y+dy] = 'x'
+            if dx == 1:
+                dfs(x+dx, y+dy, 1)
+            else:
+                dfs(x+dx, y+dy, 0)
             if flag:
                 break
             else:
-                visited[x+dx][y+dy] = 0
+                MAP[x+dx][y+dy] = '.'
 
 
 R, C = map(int, input().split())
@@ -33,14 +38,19 @@ visited = [[0] * C for _ in range(R)]
 flag = 0
 ans = 0
 for idx in range(R):
-    visited[idx][0] = 1
-    dfs(0, idx)
+    if MAP[idx][0] == 'x':
+        continue
+    MAP[idx][0] = 'x'
+    dfs(idx, 0, 0)
 
     if flag:
         ans += 1
         flag = 0
+    else:
+        break
 
-    for inner in visited:
+    for inner in MAP:
         print(inner)
-    print('--------------')
+    print('--------------------------------------------------------------------')
 print(ans)
+# 4
