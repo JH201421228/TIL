d4f3c104512417b36f6167094c1ea7b4afd7460b
박주헌 <741u741@naver.com>
2024-01-03 23:58:25 +0900

    20240103 #2

--- 새 폴더/pjt/src/App.js
+++ 새 폴더/pjt/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import MainPage from "./components/page/MainPage";
+import PostViewPage from "./components/page/PostViewPage";
+import PostWritePage from "./components/page/PostWritepage";
+
+const MainTitleText = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+function App(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <MainTitleText>20240103</MainTitleText>
+      <Routes>
+        <Route index element={<MainPage/>}/>
+        <Route path="post-write" element={<PostWritePage/>}/>
+        <Route path="post/:postId" element={<PostViewPage/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
