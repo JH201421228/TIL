721ab3878f8c35c1f9fbe938e3eb498cfab10dc4
박주헌 <741u741@naver.com>
2024-01-11 23:57:14 +0900

    20240111 #2

--- redux/real/src/pages/ChatPage/SidePanel/ChatRooms.jsx
+++ redux/real/src/pages/ChatPage/SidePanel/ChatRooms.jsx
@@ -1,12 +1,85 @@
-import React, { useState } from 'react'
-import { Button, Modal } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Button, Modal, Form } from 'react-bootstrap'
 import {FaPlus, FaRegSmileWink} from 'react-icons/fa'
-import { Form } from 'react-router-dom'
+import {DataSnapshot, child, ref as dbRef, onChildAdded, push, update} from 'firebase/database'
+import {db} from '../../../firebase'
+import { useDispatch, useSelector } from 'react-redux'
 
 const ChatRooms = () => {
   const [show, setShow] = useState(false)
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
+
+  const chatRoomsRef = dbRef(db, "chatRooms")
+
+  const [chatRooms, setChatRooms] = useState([])
+  const [firstLoad, setFirstLoad] = useState(true)
+  const [activeChatRoomId, setActiveChatRoomId] = useState('')
+
+  const {currentUser} = useSelector((state) => state.user)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    addChatRoomsListners()
+  }, [])
+
+  const handleSubmit = async () => {
+
+    if (isFormValid(name, description)) {
+
+      const key = push(chatRoomsRef).key
+
+      const newChatRoom = {
+        id: key,
+        name: name,
+        description: description,
+        createdBy: {
+          name: currentUser.displayName,
+          image: currentUser.photoURL
+        }
+      }
+
+      try {
+        await update(child(chatRoomsRef, key), newChatRoom)
+        setName('')
+        setDescription('')
+        setShow(false)
+      }
+      catch (error) {
+        alert(error)
+      }
+
+    }
+  }
+
+  const addChatRoomsListners = () => {
+    let chatRoomsArray = []
+
+    onChildAdded(chatRoomsRef, DataSnapshot => {
+      chatRoomsArray.push(DataSnapshot.val())
+      const newChatRooms = [...chatRoomsArray]
+      setChatRooms(newChatRooms)
+    })
+  }
+
+  const isFormValid = (name, description) => {
+    return name && description
+  }
+
+  const renderChatRooms = () => {
+    return (
+      chatRooms.length > 0 &&
+        chatRooms.map(room => {
+          return (
+            <li
+              key={room.id}
+            >
+              # {room.name}
+            </li>
+          )
+        })
+      )
+    }
 
   return (
     <div>
@@ -18,9 +91,14 @@ const ChatRooms = () => {
       }}>
         <FaRegSmileWink style={{marginRight: 3}} />
         CHAT ROOMS {" "}
-        <FaPlus onClick={() => setShow(!show)} />
+        <FaPlus style={{position: 'absolute', right:0, cursor: 'pointer'}} onClick={() => setShow(!show)} />
       </div>
-      <Modal>
+
+      <ul style={{listStyleType: 'none', padding: 0}}>
+        {renderChatRooms()}
+      </ul>
+
+      <Modal show={show} onHide={() => setShow(false)}>
         <Modal.Header closeButton>
           <Modal.Title>채팅 방 생성하기</Modal.Title>
         </Modal.Header>
@@ -48,7 +126,7 @@ const ChatRooms = () => {
           <Button variant='secondary' onClick={() => setShow(false)}>
             취소
           </Button>
-          <Button variant='primary' >
+          <Button variant='primary' onClick={handleSubmit} >
             생성
           </Button>
         </Modal.Footer>
