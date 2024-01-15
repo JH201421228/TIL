0782eb6fe71e3a5b83a5e6724db7c80ebbfc5d78
박주헌 <741u741@naver.com>
2024-01-16 00:08:26 +0900

    20240116 #1

--- redux/real/src/pages/ChatPage/SidePanel/DirectMessages.jsx
+++ redux/real/src/pages/ChatPage/SidePanel/DirectMessages.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {FaRegSmile} from 'react-icons/fa'
+import {ref, onChildAdded, DataSnapshot} from 'firebase/database'
+import {db} from '../../../firebase'
+import { useSelector } from 'react-redux'
 
 const DirectMessages = () => {
+
+  const usersRef = ref(db, "users")
+
+  const [activeChatRoom, setActiveChatRoom] = useState('')
+
+  const [users, setUsers] = useState([])
+
+  const {currentUser} = useSelector(state => state.user)
+
+  useEffect(() => {
+    if (currentUser?.uid) {
+      addUsersListener(currentUser.uid)
+    }
+    return () => {
+
+    }
+  }, [currentUser?.uid])
+
+  const addUsersListener = (currentUserId) => {
+    let usersArray = []
+    onChildAdded(usersRef, DataSnapshot => {
+      if (currentUserId !== DataSnapshot.key) {
+        let user = DataSnapshot.val()
+        user['uid'] = DataSnapshot.key
+        usersArray.push(user)
+
+        const newUsersArray = [...usersArray]
+        setUsers(newUsersArray)
+      }
+    })
+  }
+
+  const renderDirectMessages = (users) => {
+    return (
+      users.length > 0 &&
+      users.map(user => (
+        <li key={user.uid}
+          style={{
+            backgroundColor: user.uid === activeChatRoom ? '#ffffff45':''
+          }}
+        >
+          # {user.name}
+        </li>
+      ))
+    )
+  }
+
   return (
-    <div>DirectMessages</div>
+    <div>
+      <span style={{display: 'flex', alignItems: 'center'}}>
+        <FaRegSmile
+          style={{marginRight: 3}}
+        />
+        DIRECT MESSAGES
+      </span>
+      <ul style={{listStyleType: 'none', padding: 0}}>
+        {renderDirectMessages(users)}
+      </ul>
+    </div>
   )
 }
 
--- self/202401/20240115/boj2468/1st.py
+++ self/202401/20240115/boj2468/1st.py
@@ -6,21 +6,16 @@ input = sys.stdin.readline
 
 delta = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-def bfs(i, j):
+def bfs(i, j, n):
 
-    que = deque([0, 0])
-    visited = [[0] * N for _ in range(N)]
-    visited[0][0] = 1
-    # 처음에 0 0 이면 수정
-    check_list = []
+    que = deque([(i, j)])
+
     while que:
         x, y = que.popleft()
         for dx, dy in delta:
-            if 0 <= x+dx < N and 0 <= y+dy < N and area[x+dx][y+dy]:
-                check_list.append((x+dx, y+dy))
-
-
-
+            if 0 <= x+dx < N and 0 <= y+dy < N and not visited[x+dx][y+dy] and area[x+dx][y+dy] > n:
+                que.append((x+dx, y+dy))
+                visited[x+dx][y+dy] = 1
     return
 
 
@@ -28,13 +23,34 @@ N = int(input())
 
 area = [list(map(int, input().split())) for _ in range(N)]
 
-check_num = float('inf')
+min_num = float('inf')
+max_num = 0
 
 for i in range(N):
     for j in range(N):
-        check_num = min(check_num, area[i][j])
+        max_num = max(max_num, area[i][j])
+        min_num = min(min_num, area[i][j])
 
+# print(max_num, min_num)
 
+ans_list = []
 
-# print(check_num)
+for n in range(min_num, max_num):
+    visited = [[0] * N for _ in range(N)]
+    ans = 0
+    for i in range(N):
+        for j in range(N):
+            if area[i][j] > n and not visited[i][j]:
+                visited[i][j] = 1
+                bfs(i, j, n)
+                ans += 1
+    ans_list.append(ans)
 
+# print(ans_list)
+if ans_list:
+    print(max(ans_list))
+else:
+    print(1)
+
+# for n in range(min_num, max_num):
+#     print(n)
