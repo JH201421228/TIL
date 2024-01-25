007ab8eb54e22ced833a29b5068c9703ee0bc8b7
박주헌 <741u741@naver.com>
2024-01-25 18:02:43 +0900

    20240125

---
+++ self/202401/20240125/boj_12852/1st.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+N = int(input())
+
+memo = [0] * (N+1)
+ans = [[] for _ in range(N+1)]
+ans[1].append(1)
+
+for idx in range(2, N+1):
+    memo[idx] = memo[idx-1] + 1
+    pre = idx-1
+    if not idx % 3 and memo[idx//3]+1 < memo[idx]:
+        memo[idx] = memo[idx//3] + 1
+        pre = idx//3
+    if not idx % 2 and memo[idx//2]+1 < memo[idx]:
+        memo[idx] = memo[idx//2] + 1
+        pre = idx//2
+    ans[idx] = [idx] + ans[pre]
+
+print(memo[N])
+print(*ans[N])
