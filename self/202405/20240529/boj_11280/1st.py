ac58b89ca358878de5e219ea6d34fcd234e5870d
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-29 13:48:23 +0900

    20240529 #1

---
+++ self/202405/20240529/boj_11280/1st.py
@@ -0,0 +1,54 @@
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+sys.setrecursionlimit(100_000)
+
+
+def find_scc(now):
+    order[0] += 1
+    parent = visited[now] = order[0]
+    stack.append(now)
+
+    for next in graph[now]:
+        if not visited[next]:
+            parent = min(parent, find_scc(next))
+        elif not finished[next]:
+            parent = min(parent, visited[next])
+
+    if parent == visited[now]:
+        temp = []
+        while stack:
+            out = stack.pop()
+            finished[out] = 1
+            temp.append(abs(out))
+            if now == out:
+                ans.append(temp)
+                break
+
+    return parent
+
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(2*N+1)]
+finished = [0] * (2*N + 1)
+visited = [0] * (2*N + 1)
+order = [0]
+stack = []
+ans = []
+
+for _ in range(M):
+    p1, p2 = map(int, input().split())
+    graph[p1].append(-p2)
+    graph[p2].append(-p1)
+
+for idx in range(-N, N+1):
+    if idx and not visited[idx]:
+        find_scc(idx)
+
+for inner in ans:
+    inner_set = set(inner)
+    for n in inner:
+        if len(inner_set) != len(inner):
+            print(0)
+            exit(0)
+print(1)
