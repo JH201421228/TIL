6905149023ad7ecd973682873586476b71fbb784
박주헌 <741u741@naver.com>
2025-02-07 20:55:54 +0900

    20250207 #2

--- self/202502/20250207/boj_32982/1st.py
+++ self/202502/20250207/boj_32982/1st.py
@@ -11,27 +11,27 @@ meal = 1
 
 while True:
     if meal % 3 == 0:
-        if (cur + K) % 1440 >= times[1]:
+        if cur + K >= 1440 and (cur + K) % 1440 >= times[1]:
             cur = times[1]
-        elif (cur + K) % 1440 >= times[0]:
+        elif cur + K >= 1440 and (cur + K) % 1440 >= times[0]:
             cur += K
         else:
             print("NO")
             break
 
     elif meal % 3 == 1:
-        if (cur + K) >= times[3]:
+        if cur + K >= times[3]:
             cur = times[3]
-        elif (cur + K) >= times[2]:
+        elif cur + K >= times[2]:
             cur += K
         else:
             print("NO")
             break
 
     else:
-        if (cur + K) >= times[5]:
+        if cur + K >= times[5]:
             cur = times[5]
-        elif (cur + K) >= times[4]:
+        elif cur + K >= times[4]:
             cur += K
         else:
             print("NO")
@@ -40,6 +40,6 @@ while True:
     cur %= 1440
     meal += 1
 
-    if meal // 3 == N-1:
+    if meal // 3 == N:
         print("YES")
         break
