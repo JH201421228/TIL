37679aec4fcf119c66f76f161d77a2d5526ad208
JH201421228 <139435006+JH201421228@users.noreply.github.com>
2025-03-06 17:45:43 +0900

    20250306 #1

---
+++ self/202503/20250306/boj_11408/1st.py
@@ -0,0 +1,79 @@
+import sys
+from collections import deque
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+
+N, M = map(int, input().split())
+
+T, src, sink = N+M+3, N+M+1, N+M+2
+
+G = [[] for _ in range(T)]
+F = [[0] * T for _ in range(T)]
+D = [[0] * T for _ in range(T)]
+C = [[0] * T for _ in range(T)]
+
+for u in range(1, N+1):
+    G[src].append(u)
+    G[u].append(src)
+    C[src][u] = 1
+
+for u in range(N+1, N+M+1):
+    G[sink].append(u)
+    G[u].append(sink)
+    C[u][sink] = 1
+
+for u in range(1, N+1):
+    temp = list(map(int, input().split()))
+
+    for idx in range(len(temp)//2):
+        v, c = temp[idx*2+1]+N, temp[idx*2+2]
+
+        G[u].append(v)
+        G[v].append(u)
+
+        C[u][v] = 1
+        D[u][v] = c
+        D[v][u] = -c
+
+ans = 0
+
+while True:
+    pre, dist, checker = [-1] * T, [float("inf")] * T, [0] * T
+
+    q = deque([src])
+    dist[src] = 0
+    checker[src] = 1
+
+    while q:
+        n = q.popleft()
+        checker[n] = 0
+
+        for x in G[n]:
+            if C[n][x] - F[n][x] > 0 and dist[x] > dist[n] + D[n][x]:
+                dist[x] = dist[n] + D[n][x]
+                pre[x] = n
+
+                if not checker[x]:
+                    q.append(x)
+                    checker[x] = 1
+
+    if pre[sink] == -1:
+        break
+
+    n = sink
+    while n != src:
+        F[pre[n]][n] += 1
+        F[n][pre[n]] -= 1
+
+        ans += D[pre[n]][n]
+
+        n = pre[n]
+
+cnt = 0
+for inner in F:
+    if inner[sink]:
+        cnt += 1
+
+print(cnt)
+print(ans)
---
+++ self/202503/20250306/boj_2108/1st.py
@@ -0,0 +1,46 @@
+import sys
+import heapq
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+
+N = int(input())
+
+sum_n = 0
+max_n = -float("inf")
+min_n = float("inf")
+nums = [0] * 8_001
+k = 0
+h = []
+
+for _ in range(N):
+    n = int(input())
+
+    sum_n += n
+
+    max_n = max(n, max_n)
+    min_n = min(n, min_n)
+
+    nums[n] += 1
+    k = max(k, nums[n])
+
+    h.append(n)
+
+heapq.heapify(h)
+for _ in range(N//2):
+    heapq.heappop(h)
+
+cnt = 0
+mid = 0
+for idx in range(-4_000, 4_001):
+    if nums[idx] == k:
+        cnt += 1
+        mid = idx
+
+    if cnt == 2:
+        break
+
+print(round(sum_n / N))
+print(heapq.heappop(h))
+print(idx)
+print(max_n - min_n)
