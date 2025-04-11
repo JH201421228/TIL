9868ae435227cdf1042b632435359b22529a5881
박주헌 <741u741@naver.com>
2025-04-12 02:09:42 +0900

    20250411 #2

--- self/202504/20250411/boj_1474/1st.py
+++ self/202504/20250411/boj_1474/1st.py
@@ -15,15 +15,30 @@ interval = M - words_length
 share_n = interval // (N-1)
 remainder_n = interval % (N-1)
 
-ans = ('-' * share_n).join(words)
+if not remainder_n:
+    print(('_' * share_n).join(words))
+    exit(0)
+
+ans = ('_' * share_n).join(words)
 temp_len = len(ans)
 
-for idx in range(temp_len, 0, -1):
-    pass
+cur = 0
+for idx in range(temp_len-1):
+    if ans[idx+cur] == '_' and ord(ans[idx+cur+1]) > 96:
+        ans = ans[:idx+cur] + '_' + ans[idx+cur:]
+        remainder_n -= 1
+        cur += 1
+
+    if not remainder_n:
+        break
+else:
+    temp_len = len(ans)
+    for idx in range(temp_len-1, 1, -1):
+        if ans[idx-1] == '_' and ord(ans[idx]) < 91:
+            ans = ans[:idx] + '_' + ans[idx:]
+            remainder_n -= 1
+
+        if not remainder_n:
+            break
 
 print(ans)
-
-print(words_length)
-print(share_n, remainder_n)
-
-print(ord('A'), ord('Z'), ord('a'), ord('z'))
