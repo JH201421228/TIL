b69fb31280042a626c2b4338cacb2ad33ed511b9
JH201421228 <741u741@gmail.com>
2025-08-01 18:15:40 +0900

    20250801 #2

--- self/202508/boj_4994/1st.py
+++ self/202508/boj_4994/1st.py
@@ -1,20 +1,39 @@
 import sys
+from collections import deque
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 
 def solve(N):
-    M = 1
 
-    while M < N:
-        M *= 10
-        M += 1
+    if N == 1:
+        print(1)
+        return
 
-    while True:
-        pass
+    V = [0] * N
+    V[1] = 1
+    q = deque([1])
+
+    while q:
+        n = q.popleft()
+
+        if not (n * 10) % N:
+            print(n * 10)
+            return
+        else:
+            if not V[(n * 10) % N]:
+                V[(n * 10) % N] = 1
+                q.append((n * 10))
+
+        if not (n * 10 + 1) % N:
+            print(n * 10 + 1)
+            return
+        else:
+            if not V[(n * 10 + 1) % N]:
+                V[(n * 10 + 1) % N] = 1
+                q.append((n * 10 + 1))
 
     return
-
 
 def init():
     while True:
