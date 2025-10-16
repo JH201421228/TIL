11d8bcea07a602238254bbbe23dd367bd6e6c01d
JH201421228 <741u741@gmail.com>
2025-10-16 22:14:11 +0900

    20251016 #2

--- self/202510/20251016/boj_13018/1st.py
+++ self/202510/20251016/boj_13018/1st.py
@@ -4,6 +4,24 @@ input = sys.stdin.readline
 
 
 def solve():
+    N, K = map(int, input().split())
+    
+    if N == K:
+        print("Impossible")
+        return
+    
+    ans = []
+    
+    for n in range(2, 2+N-K-1):
+        ans.append(n)
+        
+    ans.append(1)
+    
+    for n in range(2+N-K-1, N+1):
+        ans.append(n)
+        
+    print(*ans)
+    
     return
 
 
