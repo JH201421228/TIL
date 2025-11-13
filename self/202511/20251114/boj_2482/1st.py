3feada0ec9560c8d18b4f8d057d0d69067937133
JH201421228 <741u741@gmail.com>
2025-11-13 23:49:03 +0900

    20251113 #3

---
+++ self/202511/20251114/boj_2482/1st.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+
+MOD = 1_000_000_003
+
+
+def solve():
+    N, K = map(int, input().split())
+
+    
+    return
+
+
+def main():
+    solve()
+
+    return
+
+
+if __name__ == "__main__":
+    main()
