651879cf49578078c836d26a31ca63072725b058
JH201421228 <741u741@gmail.com>
2025-11-14 11:25:03 +0900

    20251114 #1

--- self/202511/20251114/boj_2482/1st.py
+++ self/202511/20251114/boj_2482/1st.py
@@ -7,9 +7,24 @@ MOD = 1_000_000_003
 
 
 def solve():
-    N, K = map(int, input().split())
+    N = int(input())
+    K = int(input())
 
+    dp = [[0] * (K+1) for _ in range(N+1)]
     
+    for i in range(N+1):
+        dp[i][0] = 1
+        dp[i][1] = i
+
+    for n in range(2, N):
+        for k in range(2, K+1):
+            dp[n][k] = (dp[n-2][k-1] + dp[n-1][k]) % MOD
+
+    print((dp[N-3][K-1] + dp[N-1][K]) % MOD)
+
+    for d in dp:
+        print(d)
+
     return
 
 
