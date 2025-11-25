3da062cf7f308dcef056bd30b90a54f16a74168a
JH201421228 <741u741@gmail.com>
2025-11-25 13:44:00 +0900

    20251125 #1

---
+++ self/202511/20251125/boj_31430/1st.py
@@ -0,0 +1,40 @@
+import sys
+sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+
+def solve():
+    if int(input()) == 1:
+        A, B = map(int, input().split())
+        
+        res = ''
+        cur = A+B
+        
+        for _ in range(13):
+            res += chr(cur % 26 + 97)
+            cur //= 26
+            
+        print(res, flush=True)
+    
+    else:
+        cur = input().rstrip()
+        
+        res = 0
+        
+        for idx in range(13):
+            res += (ord(cur[idx]) - 97) * (13**idx)
+    
+        print(res)
+        
+    return
+
+
+def main():
+    
+    solve()
+    
+    return
+
+
+if __name__ == "__main__":
+    main()
