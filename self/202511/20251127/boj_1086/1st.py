7ceafacbc4e03fc9c60d8b6468c24249b759805b
JH201421228 <741u741@gmail.com>
2025-11-27 17:37:38 +0900

    20251127 #1

---
+++ self/202511/20251127/boj_1086/1st.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+
+def solve():
+    return
+
+
+def main():
+    solve()
+
+    return
+
+
+if __name__ == "__main__":
+    main()
