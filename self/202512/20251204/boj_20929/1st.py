c63007e9c23f5b707337e9391819534b5ee7c8f3
JH201421228 <741u741@gmail.com>
2025-12-04 23:57:28 +0900

    20251204 #3

--- self/202512/20251204/boj_20929/1st.py
+++ self/202512/20251204/boj_20929/1st.py
@@ -6,6 +6,24 @@ input = sys.stdin.readline
 def solve():
     N = int(input())
     
+    a_s, a_e, b_s, b_e = 1, N, 1, N
+    
+    while True:
+        a_mid = (a_s + a_e) >> 1
+        b_mid = (b_s + b_e) >> 1
+        
+        print(f"? A {a_mid}",flush=True)
+        a_res = int(input())
+        
+        print(f"? B {b_mid}",flush=True)
+        b_res = int(input())
+        
+        if a_mid == b_mid:
+            print(f"! {a_mid}")
+            break
+        
+        
+    
     return
 
 
