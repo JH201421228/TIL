55197a34d58e9c8cf24797ed7727289b9f11ba12
JH201421228 <741u741@gmail.com>
2026-01-26 22:49:37 +0900

    20260126 #2

--- self/202601/20260126/boj_18768/1st.py
+++ self/202601/20260126/boj_18768/1st.py
@@ -4,6 +4,41 @@ input = sys.stdin.readline
 
 
 def solve():
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    
+    AA, BB = [], []
+    ans = 0
+    for idx in range(N):
+        if A[idx] >= B[idx]:
+            AA.append(A[idx]-B[idx])
+            ans += A[idx]
+        else:
+            BB.append(B[idx]-A[idx])
+            ans += B[idx]
+            
+    AA.sort(reverse=True)
+    BB.sort(reverse=True)
+    
+    len_a, len_b = len(AA), len(BB)
+    
+    if abs(len_a - len_b) <= K:
+        print(ans)
+        return
+    
+    if len_a > len_b:
+        while len_a - len_b > K:
+            len_a -= 1
+            len_b += 1
+            ans -= AA.pop()
+    else:
+        while len_b - len_a > K:
+            len_b -= 1
+            len_a += 1
+            ans -= BB.pop()
+    
+    print(ans)
     
     return
 
